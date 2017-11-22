576683a78c83dae87afb03c4201d91e6b29ce208
jolan <jolan@users.noreply.github.com>
2017-11-22 11:09:09 -0600

    add config/logging/limits

--- main.go
+++ main.go
@@ -5,144 +5,234 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	"html/template"
 	"io/ioutil"
 	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
 	"time"
 
 	"net"
 
-	"github.com/decred/dcrd/chaincfg"
 	"github.com/decred/dcrd/dcrutil"
 	"github.com/decred/dcrd/rpcclient"
 )
 
-// Settings for daemon
-var dcrwCertPath = ("/home/user/.dcrwallet/rpc.cert")
-var dcrwServer = "127.0.0.1:19110"
-var dcrwUser = "USER"
-var dcrwPass = "PASSWORD"
+var (
+	cfg          *config
+	dailyPercent = float64(10)
+)
+
+// Balance and limits
+var dailyLimit float64
+var lastBalance float64
+var transactionLimit float64
 
 // Daemon Params to use
-var activeNetParams = &chaincfg.TestNet2Params
 var dcrwClient *rpcclient.Client
 
 // Map of received IP requests for funds.
-var requestedIps map[string]time.Time
-var ipTimeoutValue = 10 * time.Minute // 10 minutes
+var requestAmounts map[int64]float64
+var requestIPs map[string]int64
 
-// Webserver settings
-var listeningPort = ":8001"
+type jsonResponse struct {
+	Txid  string
+	Error string
+}
 
 // Overall Data structure given to the template to render
 type testnetFaucetInfo struct {
+	Address     string
+	Amount      float64
 	BlockHeight int64
-	Balance     int64
+	Balance     float64
+	DailyLimit  float64
 	Error       error
+	Limit       int64
+	SentToday   float64
 	Success     string
-}
-
-var funcMap = template.FuncMap{
-	"minus": minus,
-}
-
-func minus(a, b int) int {
-	return a - b
 }
 
 func requestFunds(w http.ResponseWriter, r *http.Request) {
 	fp := filepath.Join("public/views", "design_sketch.html")
+	amountSentToday := calculateAmountSentToday()
+	testnetFaucetInformation := &testnetFaucetInfo{
+		Address:    cfg.WalletAddress,
+		Amount:     cfg.WithdrawalAmount,
+		Balance:    lastBalance,
+		DailyLimit: dailyLimit,
+		Limit:      cfg.WithdrawalTimeLimit,
+		SentToday:  amountSentToday,
+	}
+
 	tmpl, err := template.New("home").ParseFiles(fp)
 	if err != nil {
 		panic(err)
 	}
+
+	hostIP, err := getClientIP(r)
+	if err != nil {
+		panic(err)
+	}
+
 	if r.Method == "GET" {
-		err = tmpl.Execute(w, nil)
-		if err != nil {
-			panic(err)
-		}
-	} else {
-		testnetFaucetInformation := &testnetFaucetInfo{}
-		hostIP, err := getClientIP(r)
-		if err != nil {
-			panic(err)
-		}
-		timeOut, ok := requestedIps[hostIP]
-		if !ok {
-			requestedIps[hostIP] = time.Now()
-		} else {
-			// If time saved in the requestedIps map is less than
-			// ten minutes later than don't allow request
-			if time.Since(timeOut) < ipTimeoutValue {
-				err = fmt.Errorf("To ensure everyone has equal access to testnet "+
-					"coins, we have a timeout per IP address of %s."+
-					" Please try again shortly", ipTimeoutValue.String())
-				testnetFaucetInformation.Error = err
-				err = tmpl.Execute(w, testnetFaucetInformation)
-				if err != nil {
-					panic(err)
-				}
+		sendReply(w, r, tmpl, testnetFaucetInformation, nil)
+		return
+	}
+
+	if err := r.ParseForm(); err != nil {
+		sendReply(w, r, tmpl, testnetFaucetInformation, err)
+		return
+	}
+
+	addressInput := r.FormValue("address")
+	amount := cfg.WithdrawalAmount
+	amountInput := r.FormValue("amount")
+	overridetokenInput := r.FormValue("overridetoken")
+
+	// enforce ratelimit unless overridetoken was specified and matches
+	if overridetokenInput != cfg.OverrideToken {
+		lastRequestTime, found := requestIPs[hostIP]
+		if found {
+			nextAllowedRequest := lastRequestTime + cfg.WithdrawalTimeLimit
+			coolDownTime := nextAllowedRequest - time.Now().Unix()
+
+			if coolDownTime >= 0 {
+				err = fmt.Errorf("You may only withdraw %v DCR every "+
+					"%v seconds.  Please wait another %v seconds.",
+					cfg.WithdrawalAmount, cfg.WithdrawalTimeLimit, coolDownTime)
+				sendReply(w, r, tmpl, testnetFaucetInformation, err)
 				return
 			}
 		}
-		r.ParseForm()
-		address := r.FormValue("address")
-		addr, err := dcrutil.DecodeAddress(address)
-		if err != nil {
-			testnetFaucetInformation.Error = err
-		} else if addr.IsForNet(activeNetParams) {
-			resp, err := dcrwClient.SendToAddress(addr, 10000000000)
-			if err != nil {
-				testnetFaucetInformation.Error = err
+	}
 
-			} else {
-				testnetFaucetInformation.Success = resp.String()
-			}
-		} else {
-			testnetFaucetInformation.Error = fmt.Errorf("address "+
-				"%s is not for %s", addr, activeNetParams.Name)
-		}
-		err = tmpl.Execute(w, testnetFaucetInformation)
+	// check amount if specified
+	if overridetokenInput == cfg.OverrideToken && amountInput != "" {
+		amount, err = strconv.ParseFloat(amountInput, 32)
 		if err != nil {
-			panic(err)
+			err = fmt.Errorf("amount invalid: %v", err)
+			sendReply(w, r, tmpl, testnetFaucetInformation, err)
+			return
 		}
 	}
+
+	// enforce the transaction limit unconditionally
+	if amount > transactionLimit {
+		err = errors.New("amount exceeds limit")
+		sendReply(w, r, tmpl, testnetFaucetInformation, err)
+		return
+	}
+
+	// enforce the daily limit unconditionally
+	if amountSentToday+amount >= dailyLimit {
+		err = errors.New("daily limit reached")
+		sendReply(w, r, tmpl, testnetFaucetInformation, err)
+		return
+	}
+
+	// Decode address and amount and send transaction.
+	address, err := dcrutil.DecodeAddress(addressInput)
+	if err != nil {
+		log.Errorf("ip %v submitted bad address %v", hostIP, addressInput)
+		sendReply(w, r, tmpl, testnetFaucetInformation, err)
+		return
+	}
+
+	if !address.IsForNet(activeNetParams.Params) {
+		err = fmt.Errorf("address "+
+			"%s is not for %s", address, activeNetParams.Name)
+		sendReply(w, r, tmpl, testnetFaucetInformation, err)
+		return
+	}
+
+	dcramount, err := dcrutil.NewAmount(amount)
+	if err != nil {
+		sendReply(w, r, tmpl, testnetFaucetInformation, err)
+		return
+	}
+
+	resp, err := dcrwClient.SendToAddress(address, dcramount)
+	if err != nil {
+		log.Errorf("error sending %v to %v for %v: %v",
+			amount, address, hostIP, err)
+		sendReply(w, r, tmpl, testnetFaucetInformation, err)
+		return
+	}
+
+	testnetFaucetInformation.Success = resp.String()
+	testnetFaucetInformation.SentToday = testnetFaucetInformation.SentToday + amount
+	requestAmounts[time.Now().Unix()] = amount
+	requestIPs[hostIP] = time.Now().Unix()
+	log.Infof("successfully sent %v to %v for %v",
+		amount, address, hostIP)
+	updateBalance(dcrwClient)
+
+	sendReply(w, r, tmpl, testnetFaucetInformation, nil)
+}
+
+func calculateAmountSentToday() float64 {
+	amountToday := float64(0)
+
+	for then, amount := range requestAmounts {
+		now := time.Now().Unix()
+		if now-then >= 24*60*60 {
+			continue
+		}
+
+		amountToday = amountToday + amount
+	}
+
+	return amountToday
 }
 
 func main() {
-	quit := make(chan struct{})
-
-	requestedIps = make(map[string]time.Time)
-	var dcrwCerts []byte
-	dcrwCerts, err := ioutil.ReadFile(dcrwCertPath)
+	// Load configuration and parse command line.  This function also
+	// initializes logging and configures it accordingly.
+	loadedCfg, _, err := loadConfig()
 	if err != nil {
-		fmt.Printf("Failed to read dcrd cert file at %s: %s\n", dcrwCertPath,
+		return
+	}
+
+	cfg = loadedCfg
+
+	quit := make(chan struct{})
+	requestAmounts = make(map[int64]float64)
+	requestIPs = make(map[string]int64)
+
+	dcrwCerts, err := ioutil.ReadFile(cfg.WalletCert)
+	if err != nil {
+		log.Errorf("Failed to read dcrd cert file at %s: %s", cfg.WalletCert,
 			err.Error())
 		os.Exit(1)
 	}
-	fmt.Printf("Attempting to connect to dcrd RPC %s as user %s "+
-		"using certificate located in %s\n",
-		dcrwServer, dcrwUser, dcrwCertPath)
+	log.Infof("Attempting to connect to dcrd RPC %s as user %s "+
+		"using certificate located in %s",
+		cfg.WalletHost, cfg.WalletUser, cfg.WalletCert)
 	connCfgDaemon := &rpcclient.ConnConfig{
-		Host:         dcrwServer,
+		Host:         cfg.WalletHost,
 		Endpoint:     "ws",
-		User:         dcrwUser,
-		Pass:         dcrwPass,
+		User:         cfg.WalletUser,
+		Pass:         cfg.WalletPassword,
 		Certificates: dcrwCerts,
 		DisableTLS:   false,
 	}
 	dcrwClient, err = rpcclient.New(connCfgDaemon, nil)
 	if err != nil {
-		fmt.Printf("Failed to start dcrd rpcclient: %s\n", err.Error())
+		log.Errorf("Failed to start dcrd rpcclient: %s", err.Error())
 		os.Exit(1)
 	}
+	updateBalance(dcrwClient)
+	dailyLimit = lastBalance / dailyPercent
+	log.Infof("daily limit %v per transaction limit %v", dailyLimit, transactionLimit)
 
 	go func() {
 		<-quit
-		fmt.Printf("\nClosing testnet demo.\n")
+		log.Info("Closing testnet demo.")
 		dcrwClient.Disconnect()
 		os.Exit(1)
 	}()
@@ -151,9 +241,29 @@ func main() {
 	http.Handle("/fonts/", http.StripPrefix("/fonts/", http.FileServer(http.Dir("public/fonts/"))))
 	http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("public/images/"))))
 	http.HandleFunc("/", requestFunds)
-	err = http.ListenAndServe(listeningPort, nil)
+	err = http.ListenAndServe(cfg.Listen, nil)
 	if err != nil {
-		fmt.Printf("Failed to bind http server: %s\n", err.Error())
+		log.Errorf("Failed to bind http server: %s", err.Error())
+	}
+}
+
+func sendReply(w http.ResponseWriter, r *http.Request, tmpl *template.Template, info *testnetFaucetInfo, err error) {
+	jsonResp := &jsonResponse{}
+
+	if err != nil {
+		info.Error = err
+		jsonResp.Error = err.Error()
+		jsonResp.Txid = ""
+	} else {
+		jsonResp.Error = ""
+		jsonResp.Txid = info.Success
+	}
+
+	json, _ := json.Marshal(jsonResp)
+	w.Header().Set("X-Json-Reply", string(json))
+	err = tmpl.Execute(w, info)
+	if err != nil {
+		panic(err)
 	}
 }
 
@@ -163,7 +273,7 @@ func main() {
 func getClientIP(r *http.Request) (string, error) {
 	xRealIP := r.Header.Get("X-Real-IP")
 	if len(xRealIP) == 0 {
-		fmt.Println(`"X-Real-IP" header invalid, using RemoteAddr instead`)
+		log.Warn(`"X-Real-IP" header invalid, using RemoteAddr instead`)
 		host, _, err := net.SplitHostPort(r.RemoteAddr)
 		if err != nil {
 			return "", err
@@ -172,4 +282,22 @@ func getClientIP(r *http.Request) (string, error) {
 	}
 
 	return xRealIP, nil
+}
+
+func updateBalance(c *rpcclient.Client) {
+	// calculate balance
+	gbr, err := c.GetBalance("default")
+	if err != nil {
+		log.Warnf("unable to update balance: %v", err)
+		return
+	}
+
+	spendable := float64(0)
+	for _, v := range gbr.Balances {
+		spendable = v.Spendable + spendable
+	}
+
+	log.Infof("updating balance from %v to %v", lastBalance, spendable)
+	lastBalance = spendable
+	transactionLimit = spendable / 100
 }
